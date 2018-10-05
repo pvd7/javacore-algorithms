be702ede83a048e4cb0916ebe5f8a14cc11efb34
Pavel Dymov <pvdymov@vankoroil.ru>
2018-10-05 17:20:46 +0700

    оптимизация Array.sortBubble()

--- src/main/java/ru/javacore2/algorithm/lesson2/Array.java
+++ src/main/java/ru/javacore2/algorithm/lesson2/Array.java
@@ -165,7 +165,7 @@ public class Array {
     public void sortBubble() {
         boolean isSwapped = false;
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size - 1; j++) {
+            for (int j = 0; j < size - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(j, j + 1);
                     isSwapped = true;
