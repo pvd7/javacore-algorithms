55aebb409848126414cbf286e405ee59ca2f7d5d
Pavel Dymov <pvdymov@vankoroil.ru>
2018-10-15 18:03:06 +0700

    + pow() возведение числа в степень

---
+++ src/main/java/ru/javacore2/algorithm/lesson5/util/Math.java
@@ -0,0 +1,26 @@
+package ru.javacore2.algorithm.lesson5.util;
+
+public class Math {
+
+    /**
+     * Возведение в степень
+     * Временная сложность алгоритма - O(log n) логарифмическое время,
+     * т.к. при каждой итерации степень уменьшается вдвое.
+     *
+     * @param value что возвести в степень
+     * @param power степень
+     * @return результат
+     */
+    public static double pow(double value, int power) {
+        if (power == 0) return 1.0;
+        if (power == 1) return value;
+        if (power == 2) return value * value;
+
+        if (power % 2 == 0) {
+            return pow(value * value, power / 2);
+        } else {
+            return pow(value * value, power / 2) * value;
+        }
+    }
+
+}
